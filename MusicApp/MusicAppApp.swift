c4a89a84c9104198928bcea7fc884f5c11c1b003
Libranner <libranner@gmail.com>
2021-09-24 00:01:58 +0200

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Libranner Leonel Santos Espinal on 24/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
